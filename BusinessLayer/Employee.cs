93afff4aff76f6636244bf301db00eeac5225aa8
Robin <nozrulgithub@gmail.com>
2019-11-18 01:01:39 +0600

    Practiced updating editing database table with business object

---
+++ BusinessLayer/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace BusinessLayer
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        [Required]
+        public string EmployeeName { get; set; }
+        [Required]
+        public string EmployeeGender { get; set; }
+        [Required]
+        public string EmployeeCity { get; set; }
+        [Required]
+        public DateTime? DateOfBirth { get; set; }
+
+    }
+}
---
+++ BusinessLayer/EmployeeBusinessLayer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+
+namespace BusinessLayer
+{
+    public class EmployeeBusinessLayer
+    {
+        string connectionString = WebConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+        public IEnumerable<Employee> Employees
+        {
+            get
+            {
+                List<Employee> allEmployees = new List<Employee>();
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = "SELECT * FROM tblEmploye";
+                    cmd.Connection = connection;
+                    connection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Employee aEmployee = new Employee();
+                        aEmployee.EmployeeId = (int)reader["EmployeeId"];
+                        aEmployee.EmployeeName = reader["EmployeeName"].ToString();
+                        aEmployee.EmployeeGender = reader["EmployeeGender"].ToString();
+                        aEmployee.EmployeeCity = reader["EmployeeCity"].ToString();
+                        if (!(reader["DateOfBirth"] is DBNull ))
+                        {
+                            aEmployee.DateOfBirth = DateTime.Parse(reader["DateOfBirth"].ToString());
+                        }
+                       
+                        allEmployees.Add(aEmployee);
+                    }
+                    reader.Close();
+                    connection.Close();
+                    return allEmployees;
+                }
+            }
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = "sp_SaveEmployee";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@EmployeeName", employee.EmployeeName);
+                cmd.Parameters.AddWithValue("@EmployeeGender", employee.EmployeeGender);
+                cmd.Parameters.AddWithValue("@EmployeeCity", employee.EmployeeCity);
+                cmd.Parameters.AddWithValue("@DateOfBirth", employee.DateOfBirth);
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
